985fcb999e2858fe013696d500ce6f3730aaefe6
Fred Rajaona <frajaona@gmail.com>
2020-09-14 10:34:38 +0100

    Remove all files

--- borg/etc/cont-finish.d/backup-host-keys.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p /config/host_keys
-for keyfile in /etc/ssh/ssh_host_* ; do
-	cp $keyfile /config/host_keys/
-done
--- mopidy/etc/cont-init.d/init-plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv sh
-
-if [ "$MOPIDY_PLUGINS" != "" ]; then
-	echo "Installing plugins $MOPIDY_PLUGINS"
-	pip3 install $MOPIDY_PLUGINS
-fi
